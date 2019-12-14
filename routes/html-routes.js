1b734df36ddda2ea605c18b1bc12cf116704fbfe
Jed Smit <jedsmit1@gmail.com>
2019-12-14 13:34:54 -0800

    added routing to html-routes

---
+++ routes/html-routes.js
@@ -0,0 +1,9 @@
+// get requests to display html pages
+
+app.get("/tables", function (req, res) {
+    res.sendFile("kaitan-sushi/html files/tables.html")
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile("kaitan-sushi/html files/reserve.html")
+});
