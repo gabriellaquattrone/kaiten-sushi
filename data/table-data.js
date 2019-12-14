abc9449c77bc803f1d0945847434fecf154df3df
Jed Smit <jedsmit1@gmail.com>
2019-12-14 11:31:34 -0800

    added data files fpr tables and wait list

---
+++ data/table-data.js
@@ -0,0 +1,12 @@
+// Table Array 
+
+tableArray = [
+    {
+        guestName: "Me",
+        guestEmail: "something@email.com",
+        guestId: "id1111",
+        guestPhone: "555-555-5555"
+    }
+];
+
+module.exports = tableArray;
