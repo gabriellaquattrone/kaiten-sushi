52d0faee6d0b0d2a8f6de0cfbb30b544169fa67a
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2019-12-14 14:19:45 -0800

    fixing paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,15 +6,15 @@ var app = express();
 
 
 module.exports = function (app) {
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname + "../index.html"));
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../index.html"));
     })
     app.get("/tables", function (req, res) {
-        res.sendFile(path.join(__dirname + "../html files/tables.html"));
+        res.sendFile(path.join(__dirname, "../html files/tables.html"));
     });
 
     app.get("/reserve", function (req, res) {
-        res.sendFile(path.join(__dirname + "../html files/reserve.html"));
+        res.sendFile(path.join(__dirname, "../html files/reserve.html"));
     });
 
 
